4aaf53d19978112f204dd18c7d2cb24d479cd4dd
jiang.wang <jiang.wang@opay-inc.com>
2023-01-04 17:52:47 +0800

    feat: css
    Signed-off-by: jiang.wang <jiang.wang@opay-inc.com>

---
+++ src/components/Tem.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+
+let tpl = ref('this is vue components')
+onMounted(() => {
+  console.log('mounted')
+})
+</script>
+<template>
+  <div>
+    <h1>{{ tpl }}</h1>
+  </div>
+</template>
+
+<style lang="scss">
+div {
+  h1 {
+    font-size: 30px;
+  }
+}
+</style>
