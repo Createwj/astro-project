e16a80e3c8f02e3e713da04b54b48eb208ff1056
jiang.wang <jiang.wang@opay-inc.com>
2023-01-05 14:32:00 +0800

    feat: astro
    Signed-off-by: jiang.wang <jiang.wang@opay-inc.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,12 @@ import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
 import vue from "@astrojs/vue";
 
+import node from '@astrojs/node';
+
 export default defineConfig({
-  integrations: [react(), svelte(), vue()]
+  integrations: [react(), svelte(), vue()],
+  // output: 'server',
+  // adapter: node({
+  //   mode: 'standalone'
+  // }),
 });
