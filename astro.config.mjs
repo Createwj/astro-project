08578b3806a0baa6777694d488586767e40b93b4
jiang.wang <jiang.wang@opay-inc.com>
2023-01-05 16:02:19 +0800

    feat: pkg
    Signed-off-by: jiang.wang <jiang.wang@opay-inc.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,8 @@ import react from "@astrojs/react";
 import svelte from "@astrojs/svelte";
 import vue from "@astrojs/vue";
 
-// import Unocss from '@unocss/astro'
-
 import Unocss from '@unocss/astro'
 import { presetIcons, presetAttributify, presetUno } from 'unocss'
-
-// import uno from 'astro-uno'
-// import { presetUno, presetAttributify } from 'unocss'
-
 import unoConfig from './uno.config.js'
 
 import node from '@astrojs/node';
@@ -21,10 +15,7 @@ export default defineConfig({
     react(),
     svelte(),
     vue(),
-    // uno({
-    //   presets: [presetUno(), presetAttributify()],
-    //   ...unoConfig
-    // })
+   
     Unocss({  
       presets: [presetAttributify(), presetUno(), presetIcons()],
       ...unoConfig
